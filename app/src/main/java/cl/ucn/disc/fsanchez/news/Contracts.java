1e45cea61fc7985c706f650bbd18590b6cf7209a
Franco Sánchez Flores <sanchezfranco222@gmail.com>
2021-11-22 13:43:18 -0300

    feat(Model): add contracts and model

--- app/src/main/java/cl/ucn/disc/fsanchez/news/Contracts.java
+++ app/src/main/java/cl/ucn/disc/fsanchez/news/Contracts.java
@@ -15,18 +15,17 @@
 
 package cl.ucn.disc.fsanchez.news;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import java.util.List;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * The Contracts of The News Project
+ * @author Franco Sanchez-Flores
  */
-public class ExampleUnitTest {
-  @Test
-  public void addition_isCorrect() {
-    assertEquals(4, 2 + 2);
-  }
+public interface Contracts {
+
+  /**
+   *
+   * @return all teh News in the backend.
+   */
+  List<News> retrieveNews();
 }
